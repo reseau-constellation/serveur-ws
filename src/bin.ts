afa10b62a282248fd7b07a1f2729413e0e8090c3
julienmalard <julien.malard@mail.mcgill.ca>
2021-11-15 13:50:05 +0100

    v0.0.4

--- src/bin.ts
+++ src/bin.ts
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
 
-import yargs from "yargs/yargs";
+import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 // import ora, { Ora } from "ora";
 import chalk from "chalk";
 
 import lancerServeur from "./serveur"
 
+
 yargs(hideBin(process.argv))
   .usage('Utilisation: $0 <commande> [options]')
   .command(['lancer [--port <port>]'], 'Lancer le serveur', (yargs) => {
      return yargs
        .option('port', {
+         alias: "p",
          describe: 'Le numéro du port de connexion.',
          type: 'string'
        })
@@ -20,7 +22,9 @@ yargs(hideBin(process.argv))
      // let spinner: Ora
      if (argv.bavard) {
        // spinner = ora(chalk.yellow(`Initialisation du serveur sur port : ${argv.port}`)).start()
-       chalk.yellow(`Initialisation du serveur sur port : ${argv.port}`)
+       console.log(
+         chalk.yellow(`Initialisation du serveur`)
+       )
      }
 
      const { port } = await lancerServeur(
