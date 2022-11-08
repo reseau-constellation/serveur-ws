dc3fb0a2c329e02bb0fe4561b0b35d4b82845f74
julienmalard <julien.malard@mail.mcgill.ca>
2022-11-08 18:13:33 +0700

    Oups !

--- src/test.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node --experimental-specifier-resolution=node
-import { proxy } from "@constl/ipa";
-
-// https://www.codeheroes.fr/2020/10/02/profiler-son-application-node-js-analyse-des-performances-cpu/
-
-const client = proxy.ipa.générerProxyProc()
-
-const idBd = await client.bds!.créerBd({licence: "ODbl-1_0"})
-console.log({idBd});
