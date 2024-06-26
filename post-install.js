874cbcfd0e7e588d206582a38e106790a162ec1a
julienmalard <julien.malard@mail.mcgill.ca>
2024-06-26 14:17:55 +0200

    Test

--- post-install.js
+++ post-install.js
@@ -2,17 +2,15 @@ import { writeFileSync, readFileSync } from "fs";
 import { execSync } from "child_process";
 import { join, dirname } from "path";
 import { execArgv, exit } from "process";
-console.log(execArgv);
-if (!execArgv.includes("-g") || !execArgv.includes("--global")) exit(0);
+
+// if (!execArgv.includes("-g") || !execArgv.includes("--global")) exit(0);
 
 const pkgJsonPath = join(
   dirname(execSync("pnpm root -g").toString()),
   "package.json",
 );
-console.log(pkgJsonPath);
 
 const pkgJson = JSON.parse(readFileSync(pkgJsonPath));
-console.log(pkgJson);
 
 const résolutions = JSON.parse(readFileSync("package.json"))["pnpm"]?.[
   "overrides"
@@ -23,8 +21,6 @@ pkgJson.pnpm.overrides = {
   ...(pkgJson.pnpm.overrides || {}),
   ...(résolutions || {}),
 };
-console.log(résolutions);
-console.log(pkgJson);
 
 writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
 
