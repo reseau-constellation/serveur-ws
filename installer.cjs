69cc849ad8ae865361f80df7962e6ec3fbb4864e
julienmalard <julien.malard@mail.mcgill.ca>
2024-06-27 08:13:03 +0200

    Mkdir récursif

--- installer.cjs
+++ installer.cjs
@@ -20,9 +20,9 @@ if (!pkgJson.pnpm) pkgJson.pnpm = {};
 pkgJson.pnpm.overrides = {
   ...(pkgJson.pnpm.overrides || {}),
   ...(résolutions || {}),
-};
+};  
 
-if (!existsSync(dossierPnpmGlobal)) mkdirSync(dossierPnpmGlobal); 
+if (!existsSync(dossierPnpmGlobal)) mkdirSync(dossierPnpmGlobal, {recursive: true}); 
 writeFileSync(adressePkgJson, JSON.stringify(pkgJson, null, 2));
 
 execSync("pnpm add -g @constl/serveur@latest");
